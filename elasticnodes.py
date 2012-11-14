f77a0adbdb87527fde3895358ce3747956c5f3ad
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-13 19:37:54 -0600

    Remove some now-unused functions

--- elasticnodes.py
+++ elasticnodes.py
@@ -155,52 +155,6 @@ class Node(QtGui.QGraphicsItem):
     def edges(self):
         return self.edgeList
 
-    def calculateForces(self):
-        if not self.scene() or self.scene().mouseGrabberItem() is self:
-            self.newPos = self.pos()
-            return
-    
-        # Sum up all forces pushing this item away.
-        xvel = 0.0
-        yvel = 0.0
-        for item in self.scene().items():
-            if not isinstance(item, Node):
-                continue
-
-            line = QtCore.QLineF(self.mapFromItem(item, 0, 0),
-                    QtCore.QPointF(0, 0))
-            dx = line.dx()
-            dy = line.dy()
-            l = 2.0 * (dx * dx + dy * dy)
-            if l > 0:
-                xvel += (dx * 150.0) / l
-                yvel += (dy * 150.0) / l
-
-        # Now subtract all forces pulling items together.
-        weight = (len(self.edgeList) + 1) * 10.0
-        for edge in self.edgeList:
-            if edge.sourceNode() is self:
-                pos = self.mapFromItem(edge.destNode(), 0, 0)
-            else:
-                pos = self.mapFromItem(edge.sourceNode(), 0, 0)
-            xvel += pos.x() / weight
-            yvel += pos.y() / weight
-    
-        if QtCore.qAbs(xvel) < 0.1 and QtCore.qAbs(yvel) < 0.1:
-            xvel = yvel = 0.0
-
-        sceneRect = self.scene().sceneRect()
-        self.newPos = self.pos() + QtCore.QPointF(xvel, yvel)
-        self.newPos.setX(min(max(self.newPos.x(), sceneRect.left() + 10), sceneRect.right() - 10))
-        self.newPos.setY(min(max(self.newPos.y(), sceneRect.top() + 10), sceneRect.bottom() - 10))
-
-    def advance(self):
-        if self.newPos == self.pos():
-            return False
-
-        self.setPos(self.newPos)
-        return True
-
     def boundingRect(self):
         adjust = 2.0
         return QtCore.QRectF(-10 - adjust, -10 - adjust, 23 + adjust,
