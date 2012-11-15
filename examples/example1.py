236635dca9ac4eeb08c7d801e6945f568fec3121
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-14 21:29:08 -0600

    Rename attribute to be less dumb

--- examples/example1.py
+++ examples/example1.py
@@ -4,23 +4,23 @@ import treedisplay
 import treelayout
 
 class Tree:
-    def __init__(self, node="", *children):
-        self.node = node
-        self.width = len(node)
+    def __init__(self, text="", *children):
+        self.text = text
+        self.width = len(text)
         if children: self.children = children
         else:        self.children = []
     
     def __str__(self): 
-        return "%s" % (self.node)
+        return "%s" % (self.text)
     def __repr__(self):
-        return "%s" % (self.node)
+        return "%s" % (self.text)
 
     def __getitem__(self, key):
         if isinstance(key, int) or isinstance(key, slice): 
             return self.children[key]
         if isinstance(key, str):
             for child in self.children:
-                if child.node == key: return child
+                if child.text == key: return child
 
     def __iter__(self): return self.children.__iter__()
 
