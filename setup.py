046c74aea00f45b101bf95d83a891fd09d5deb28
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-14 21:19:13 -0600

    Add setup.py file for treedisplay

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name="treedisplay",
+      description="Display widget for trees",
+      version="0.0.1",
+      author="Evan Driscoll",
+      author_email="driscoll@cs.wisc.edu",
+      maintainer="Evan Driscoll",
+      maintainer_email="driscoll@cs.wisc.edu",
+      packages=["treedisplay"],
+      package_dir = {'': 'src'},
+      license = "BSD",
+      )
+
